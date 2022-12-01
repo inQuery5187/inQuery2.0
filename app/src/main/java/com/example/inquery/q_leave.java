4f3ec5de00182148b867006be07e2a4c42a7da7f
tejjus <not.tejjusbhat@gmail.com>
2022-12-01 22:45:32 +0530

    complaint, custom, leave

---
+++ app/src/main/java/com/example/inquery/q_leave.java
@@ -0,0 +1,13 @@
+package com.example.inquery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class q_leave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qleave);
+    }
+}
