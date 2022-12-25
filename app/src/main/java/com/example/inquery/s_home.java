5ef5951f74820c599286691930043cc94afdcca4
Yash Sharma <yashsharma0558@gmail.com>
2022-12-25 21:33:21 +0530

    nav menu item select

--- app/src/main/java/com/example/inquery/s_home.java
+++ app/src/main/java/com/example/inquery/s_home.java
@@ -3,19 +3,27 @@ package com.example.inquery;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class s_home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class s_home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shome);
+        setNavigationViewListener();
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
@@ -30,6 +38,40 @@ public class s_home extends AppCompatActivity {
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+
+            case R.id.nav_account:{
+                navAccount();
+                return true;
+            }
+
+            case R.id.nav_profile:
+            {
+                Intent intent= new Intent(this, s_profile.class);
+                startActivity(intent);
+                return true;
+            }
+
+            case R.id.nav_queries:
+            {
+                Intent intents= new Intent(this, s_query.class);
+                startActivity(intents);
+                return true;
+            }
+
+            case R.id.nav_settings:{
+                navSettings();
+                return true;
+            }
+
+            }
+        //close navigation drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     // override the onOptionsItemSelected()
     // function to implement
@@ -38,10 +80,25 @@ public class s_home extends AppCompatActivity {
     // drawer when the icon is clicked
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void navAccount() {
+    }
+    private void navProfile() {
+
+    }
+    private void navQueries() {
+
+    }
+    private void navSettings() {
+    }
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
 }
