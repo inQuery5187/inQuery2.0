03a977301add61990807184c0b45453a9032bb47
[uditi sharma] <[uditi1002@gmail.com]>
2023-02-03 10:13:29 +0530

    calender added

--- app/src/main/java/com/example/inquery/s_profile.java
+++ app/src/main/java/com/example/inquery/s_profile.java
@@ -46,7 +46,7 @@ public class s_profile extends AppCompatActivity implements NavigationView.OnNav
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         userId= findViewById(R.id.userId);
-        name= findViewById(R.id.backgroundtop);
+        name= findViewById(R.id.userName);
         query= findViewById(R.id.querybox);
         query.setVisibility(View.INVISIBLE);
         reference= FirebaseDatabase.getInstance().getReference("Data").child("Faculty");
