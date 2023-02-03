7eaceb0788590514d13006d1c9401b3fb7fcdd86
Yash Sharma <yashsharma0558@gmail.com>
2023-02-03 11:11:42 +0530

    sum

---
+++ app/src/main/java/com/example/inquery/sprofileedit.java
@@ -0,0 +1,14 @@
+package com.example.inquery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sprofileedit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sprofileedit);
+    }
+}
